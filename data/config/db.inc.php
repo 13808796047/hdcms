834e548046b7730eb0a3498c803b3c40d99a57d7
彡Magic <13808796047@139.com>
2014-06-30 09:57:05 +0800

    HDCMS

---
+++ data/config/db.inc.php
@@ -0,0 +1,12 @@
+<?php if (!defined('HDPHP_PATH'))exit('No direct script access allowed');
+return array (
+  'DB_DRIVER' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => 3306,
+  'DB_USER' => 'root',
+  'DB_PASSWORD' => 'admin888',
+  'DB_DATABASE' => 'hdcms',
+  'DB_PREFIX' => 'hd_',
+  'WEB_MASTER' => 'admin',
+);
+?>
